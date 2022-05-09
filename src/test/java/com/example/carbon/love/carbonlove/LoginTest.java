c66ca989b2661b519aaedd450bf75b7e3f10c11c
Douglas Fugazi <itmteleco@gmail.com>
2022-05-09 15:23:50 -0500

    Creating a new scenario: LoginTest with standard user credentials

--- src/test/java/com/example/carbon/love/carbonlove/LoginTest.java
+++ src/test/java/com/example/carbon/love/carbonlove/LoginTest.java
@@ -1,18 +1,36 @@
 package com.example.carbon.love.carbonlove;
 
+import com.example.carbon.love.carbonlove.BaseUtility.BaseLogin;
 import com.example.carbon.love.carbonlove.Pages.LoginPage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
-public class LoginTest {
+import static com.example.carbon.love.carbonlove.BaseUtility.LoginCredentials.STANDARD_USER;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
-    private WebDriver driver;
+class LoginTest {
+
+    public WebDriver driver;
     private LoginPage loginPage;
 
+    /**
+     * Test login with valid credentials: STANDARD_USER
+     */
     @Test
-    public void loginTest() {
-        loginPage = new LoginPage(driver);
+    @DisplayName("Login with valid credentials: STANDARD_USER")
+    @Tag("Smoke")
+    void loginWithValidCredentialsStandardUser() {
+        LoginPage loginPage = new LoginPage(driver);
+        BaseLogin baseLogin = new BaseLogin(loginPage);
         loginPage.setupUrl();
+        baseLogin.authenticateStandardUser(STANDARD_USER);
+        assertSoftly(softly -> {
+            softly.assertThat(loginPage.checkAppLogo())
+                    .describedAs("Swag-lab is loaded. Heading logo is displayed")
+                    .isTrue();
+        });
         loginPage.tearDown();
     }
 }
